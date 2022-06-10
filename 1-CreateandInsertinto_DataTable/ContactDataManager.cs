8dc6bcde64f5300a58e0061d30eff7a1e8721fc7
soni240 <s.fegade21@gmail.com>
2022-06-10 21:47:41 +0530

    [Gunavanti]create and insert into data table

--- 1-CreateandInsertinto_DataTable/ContactDataManager.cs
+++ 1-CreateandInsertinto_DataTable/ContactDataManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Linq_Address_Book
+namespace _1_CreateandInsertinto_DataTable
 {
     public class ContactDataManager
     {
